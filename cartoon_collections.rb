11da0756c6c8e6e2a1d574da9b1027b2f0154c88
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-06 20:12:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def long_planeteer_calls(array)
   array.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.find { |string| string == cheese_types }
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
